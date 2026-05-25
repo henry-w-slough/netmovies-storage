a63865f7e1ffbb7a7e74cecfb75eeb66c4b3b9f4
henry-w-slough <henry.w.slough@gmail.com>
2026-05-25 11:36:03 -0400

    Added Services for /data and /directory, started abstraction process for /directory

--- Controllers/MovieDirectoryController.py
+++ Controllers/MovieDirectoryController.py
@@ -6,32 +6,35 @@ import shutil
 import config
 
 from Exceptions.MovieNotFoundException import MovieNotFoundException
+from Services import MovieDirectoryService
 
 
 class MovieDirectoryController:
 
 
     def __init__(self, app:fastapi.FastAPI) -> None:
-        """Handles all Movie directory related HTTP requests."""
+        """Handles and directs all Movie directory related HTTP requests."""
+
+        self.movie_directory_service = MovieDirectoryService.MovieDirectoryService()
 
         #adding endpoints to fastapi
-        app.post("/directory/createMovie/{storageId:uuid}")(self.create_movie_directory)
-        app.delete("/directory/deleteMovieByStorageId/{storageId:uuid}")(self.delete_movie_by_storage_id)
+        app.post("/directory/createMovie/{storage_id:uuid}")(self.create_movie_directory)
+        app.delete("/directory/deleteMovieByStorageId/{storage_id:uuid}")(self.delete_movie_by_storage_id)
 
 
-    async def create_movie_directory(self, storageId:uuid.UUID) -> fastapi.Response:
+    async def create_movie_directory(self, storage_id:uuid.UUID) -> fastapi.Response:
         #creating movie directory
-        os.makedirs(os.path.join(config.MOVIE_ROOT_DIRECTORY, f"{storageId}"), exist_ok=True)
-        return fastapi.responses.JSONResponse(status_code=201, content={"storageId": f"{storageId}"})
+        os.makedirs(os.path.join(config.MOVIE_ROOT_DIRECTORY, f"{storage_id}"), exist_ok=True)
+        return fastapi.responses.JSONResponse(status_code=201, content={"storageId": f"{storage_id}"})
     
     
-    async def delete_movie_by_storage_id(self, storageId:uuid.UUID) -> fastapi.Response:
+    async def delete_movie_by_storage_id(self, storage_id:uuid.UUID) -> fastapi.Response:
         
         #creating movie directory
-        movie_directory = os.path.join(config.MOVIE_ROOT_DIRECTORY, f"{storageId}")
+        movie_directory = os.path.join(config.MOVIE_ROOT_DIRECTORY, f"{storage_id}")
 
         if not os.path.exists(movie_directory):
-            raise MovieNotFoundException(f"Movie of StorageId: {storageId} could not be found within storage.")
+            raise MovieNotFoundException(f"Movie of StorageId: {storage_id} could not be found within storage.")
         
         #deleting the directory
         shutil.rmtree(movie_directory)
---
+++ Services/MovieDirectoryService.py
@@ -0,0 +1,13 @@
+import uuid
+
+
+class MovieDirectoryService:
+    """Handles all logic relating to directories of movies."""
+
+
+    def create_movie_directory(self, storage_id:uuid.UUID):
+        pass
+
+
+    def delete_movie_by_storage_id(self, storage_id:uuid.UUID):
+        pass
