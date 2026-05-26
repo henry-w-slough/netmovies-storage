1e5e22eeaf5dfce714f8ea7079c0b3dd4140000a
henry-w-slough <henry.w.slough@gmail.com>
2026-05-25 20:01:51 -0400

    Seperated controller to service for MovieData

--- Controllers/MovieDataController.py
+++ Controllers/MovieDataController.py
@@ -4,6 +4,7 @@ import os
 
 from Utility import MovieDataParser
 from Utility import MovieDataTranscoder
+from Services import MovieDataService
 import config
 
 
@@ -13,28 +14,26 @@ class MovieDataController:
     def __init__(self, app:fastapi.FastAPI) -> None:
         """Handles all Movie data related HTTP requests."""
 
+        self.movie_data_service = MovieDataService.MovieDataService()
+
         #adding endpoints to fastapi
-        app.post("/data/uploadMovieData/{storageId:uuid}")(self.upload_movie_data)
-        app.get("/data/downloadMovieData/{storageId:uuid}")(self.download_movie_data)
+        app.post("/data/uploadMovieData/{storage_id:uuid}")(self.upload_movie_data)
+        app.get("/data/downloadMovieData/{storage_id:uuid}")(self.download_movie_data)
 
 
-    async def upload_movie_data(self, storageId:uuid.UUID, request:fastapi.Request) -> fastapi.Response:
-
-        movie_directory = os.path.join(config.MOVIE_ROOT_DIRECTORY, str(storageId))
-
-        #attaching the movie data stream in the movie directory using MovieDataAssembler
-        await MovieDataParser.merge_stream(movie_directory, request.stream())
-
+    async def upload_movie_data(self, storage_id:uuid.UUID, request:fastapi.Request) -> fastapi.Response:
+        
+        #storageId in content is a string in order to 
         return fastapi.responses.JSONResponse(
             status_code=200,
-            content={"storageId": str(storageId)}
+            content={"storageId": str(await self.movie_data_service.upload_movie_data(storage_id, request.stream()))}
         )
 
 
-    async def download_movie_data(self, storageId:uuid.UUID) -> fastapi.Response:
+    async def download_movie_data(self, storage_id:uuid.UUID) -> fastapi.Response:
 
         return fastapi.responses.StreamingResponse(
             status_code=200,
-            content= MovieDataParser.stream_movie(f"{config.MOVIE_ROOT_DIRECTORY}/{storageId}/{config.MOVIE_FILE_NAME}"),
+            content = await self.movie_data_service.download_movie_data(storage_id),
             headers={"fileName": config.MOVIE_FILE_NAME, "fileExtension": config.MOVIE_FILE_EXTENSION} 
         )
--- Services/MovieDataService.py
+++ Services/MovieDataService.py
@@ -1,5 +1,24 @@
+import uuid
+import os
+from typing import AsyncGenerator
+
+import config
+
+import Utility.MovieDataParser as MovieDataParser
 
 
 class MovieDataService:
+    """Handles all direct logic relating to movie data."""
 
-    async def 
+
+    async def upload_movie_data(self, storage_id:uuid.UUID, data_stream:AsyncGenerator[bytes, None]) -> uuid.UUID:
+
+        #attaching the movie data stream in the connected movie directory using MovieDataAssembler
+        await MovieDataParser.merge_stream(os.path.join(config.MOVIE_ROOT_DIRECTORY, str(storage_id)), data_stream)
+
+        return storage_id
+
+
+    async def download_movie_data(self, storage_id:uuid.UUID) -> AsyncGenerator[bytes, None]:
+        
+        return MovieDataParser.stream_movie(os.path.join(config.MOVIE_ROOT_DIRECTORY, str(storage_id)))
